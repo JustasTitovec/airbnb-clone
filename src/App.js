ca27f4ef6236939d6a190173357edadffdeb49a2
Justas Titovec <justastitovec@Justass-MacBook-Pro.local>
2020-08-31 18:38:32 +0300

    layout and design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from '../src/components/Home/Home';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchPage from './components/SearchPage/SearchPage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
